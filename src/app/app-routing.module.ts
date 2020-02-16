37c3e0ee770bc0a829b8425338695247946945c1
Austin Roa <ausroa@gmail.com>
2020-02-16 02:29:11 -0500

    created layout for comic view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {ComicsComponent} from './dashboard/comics/comics.component';
+import {GamesComponent} from './dashboard/games/games.component';
+import {MusicComponent} from './dashboard/music/music.component';
+
+const routes: Routes = [
+  {
+    path: 'dashboard', component: DashboardComponent,
+    children: [
+      { path: 'comics', component: ComicsComponent },
+      { path: 'games', component: GamesComponent },
+      { path: 'music', component: MusicComponent }
+    ]
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ComicsComponent} from './dashboard/comics/comics.component';
+import {GamesComponent} from './dashboard/games/games.component';
+import {MusicComponent} from './dashboard/music/music.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ComicsComponent,
+    GamesComponent,
+    MusicComponent
   ],
   imports: [
     BrowserModule,
